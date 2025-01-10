a12f35856a17db587680b51dcdb1a47ac7ff8271
jamesmoore24 <jame.moore24@gmail.com>
2025-01-10 10:18:01 -0800

    new cname file

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { writeFile } from "fs/promises";
+import type { Plugin } from "vite";
+
+// Custom plugin to generate CNAME file
+const generateCNAME = (): Plugin => {
+  return {
+    name: "generate-cname",
+    closeBundle: async () => {
+      await writeFile("docs/CNAME", "jmoore.info");
+    },
+  };
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), generateCNAME()],
   base: "./",
   build: {
     outDir: "docs",
