e7cdcbbb5903f168771b49f7bf050a5bbbb1154c
jamesmoore24 <jame.moore24@gmail.com>
2025-01-15 09:49:36 -0500

    new build

--- src/components/CareerVisualizer.tsx
+++ src/components/CareerVisualizer.tsx
@@ -8,13 +8,6 @@ import MITSloanLogo from "/logos/mit_sloan.png";
 
 const experiences = [
   {
-    company: "Meta",
-    role: "Incoming Software Engineer",
-    period: "June 2025",
-    logo: MetaLogo,
-    link: "https://www.meta.com/",
-  },
-  {
     company: "Capital One",
     role: "Software Engineer Intern",
     period: "Jun 2024 - Aug 2024",
